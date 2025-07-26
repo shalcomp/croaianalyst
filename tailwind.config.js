98952b31c0e963001cf8b26aa4b9813df095d603
Fredrick <128906458+shalcomp@users.noreply.github.com>
2025-07-26 16:30:57 +0300

    Add files via upload
    New files changed for proper netlify deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,3 @@
-
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,30 +5,7 @@ export default {
     "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        slate: {
-          950: '#020617'
-        }
-      },
-      animation: {
-        'fade-in': 'fade-in 0.5s ease-out forwards',
-        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
-      },
-      keyframes: {
-        'fade-in': {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
-        'fade-in-up': {
-            '0%': { opacity: '0', transform: 'translateY(10px)' },
-            '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
